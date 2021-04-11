b7499b8b2003509563c5c48a9426a543046769d8
Ana Tartari <anatartari412@gmail.com>
2021-04-11 17:54:08 -0300

    Rota para retorno de dados

---
+++ GitTake/Controllers/GitGubTake.cs
@@ -0,0 +1,32 @@
+﻿using GitTake.Models;
+using GitTake.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GitTake.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitGubTake : ControllerBase
+    {
+        // GET: api/<GitGubTake>
+        [HttpGet("posicao")]
+        public async Task<CarrosselInfo> GetAsync(int posicao)
+        {
+            GitHubTakeRepo _repo = new GitHubTakeRepo();
+            List<CarrosselInfo> lista = await _repo.getGitRepos();
+
+            return lista[posicao];
+        }
+
+      
+    }
+}
