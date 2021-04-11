7b6ec93cec581e7019dbe1115cf714c05a26ad9e
Ana Tartari <anatartari412@gmail.com>
2021-04-11 17:53:04 -0300

    Model de informação

---
+++ GitTake/Models/CarrosselInfo.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GitTake.Models
+{
+    public class CarrosselInfo
+    {
+
+        [JsonPropertyName("full_name")]
+        public string Nome { get; set; }
+        [JsonPropertyName("language")]
+        public string Linguagem { get; set; }
+        [JsonPropertyName("description")]
+        public string Descricao { get; set; }
+        [JsonPropertyName("created_at")]
+        public DateTime Criado { get; set; }
+    }
+}
